4879796cc1ced6e76c45ade77c3378589c434758
MrSleep404 <287802928@qq.com>
2026-07-29 16:40:39 +0800

    添加路由切换时自动滚动到顶部功能
    - 创建ScrollToTop组件，监听路由变化并自动滚动到页面顶部
    - 在App.tsx中添加ScrollToTop组件
    - 解决进入关于我们、联系我们、隐私政策页面时页面停留在底部的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
+import ScrollToTop from './components/common/ScrollToTop'
 import Home from './pages/Home'
 import WordToMarkdown from './pages/WordToMarkdown'
 import MarkdownToHtml from './pages/MarkdownToHtml'
@@ -13,6 +14,7 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/word-to-markdown" element={<WordToMarkdown />} />
